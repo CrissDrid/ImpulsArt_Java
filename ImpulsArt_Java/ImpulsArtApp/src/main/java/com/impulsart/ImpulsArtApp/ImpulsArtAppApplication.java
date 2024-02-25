9239eb5b45cb14309949f6571eca3b2e311c5f80
Criss <cristopherm767@gmail.com>
2024-02-25 18:33:34 -0500

    First Version

---
+++ ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/ImpulsArtAppApplication.java
@@ -0,0 +1,13 @@
+package com.impulsart.ImpulsArtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpulsArtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpulsArtAppApplication.class, args);
+	}
+
+}
---
+++ ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.impulsart.ImpulsArtApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ImpulsArtAppApplication.class);
+	}
+
+}
