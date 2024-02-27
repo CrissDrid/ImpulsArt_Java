58f17df201216f26ca77a37b739808c20570e991
unknown <angel.acuna.meza@gmail.com>
2024-02-27 18:29:28 -0500

    Modulo subasta_V2 con la tabla oferta y subasta

--- ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/entities/Oferta.java
+++ ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/entities/Oferta.java
@@ -1,2 +1,33 @@
-package com.impulsart.ImpulsArtApp.entities;public class Oferta {
+package com.impulsart.ImpulsArtApp.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "Ofertas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Oferta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PkCod_oferta", nullable = false)
+    private Long PkCod_oferta;
+
+    @Column(name = "Monto", length = 255, nullable = false)
+    private int Monto;
+
+    @Column(name = "FechaOferta")
+    private LocalDate FechaOferta;
+
+    @Column(name = "HoraOferta")
+    private LocalTime HoraOferta;
+
 }
