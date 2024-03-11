d8d912d92c7d0922923f2b174b5d3f78cf938baa
Criss <crisopherm767@gmail.com>
2024-03-11 08:42:40 -0500

    UserController Create

--- ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/controller/UserController.java
+++ ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.impulsart.ImpulsArtApp.controller;
 
+
 import com.impulsart.ImpulsArtApp.entities.Usuario;
+import com.impulsart.ImpulsArtApp.service.imp.UsuarioImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,12 +17,28 @@ import java.util.Map;
 @RequestMapping(path = "/api/usuario", method = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.HEAD})
 @CrossOrigin("*")
 public class UserController {
-
+ @Autowired
+    private UsuarioImp usuarioImp;
     @PostMapping("/create")
     public ResponseEntity<Map<String,Object>> create(@RequestBody Map<String, Object> request){
         Map<String, Object> response = new HashMap<>();
         try{
             System.out.println("@@@@"+request);
+            Usuario usuario = new Usuario();
+            usuario.setIdentificacion(Integer.parseInt(request.get("identificacion").toString()));
+            usuario.setNombre(request.get("nombre").toString());
+            usuario.setApellido(request.get("apellido").toString());
+            SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+            java.sql.Date fechaAnalizada = new java.sql.Date(formateador.parse((String) request.get("fechaNacimiento")).getTime());
+            usuario.setFechaNacimiento(fechaAnalizada);
+            usuario.setEmail(request.get("email").toString());
+            usuario.setNumCelular(request.get("numCelular").toString());
+            usuario.setDireccion(request.get("direccion").toString());
+            usuario.setContrasena(request.get("contrasena").toString());
+            usuario.setTipoUsuario(request.get("tipoUsuario").toString());
+
+            this.usuarioImp.create(usuario);
+
             response.put("status","succses");
             response.put("data","Registro Exitoso");
         }catch (Exception e){
--- ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/entities/Usuario.java
+++ ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/entities/Usuario.java
@@ -17,21 +17,21 @@ public class Usuario {
     @Id
     @Column(name = "Pk_Identificacion")
     private int identificacion;
-    @Column(name = "Nombre",length = 20,nullable = false)
+    @Column(name = "Nombre", length = 20, nullable = false)
     private String nombre;
-    @Column(name = "Apellido",length = 20,nullable = false)
+    @Column(name = "Apellido", length = 20, nullable = false)
     private String apellido;
-    @Column(name = "FechaNacimiento",nullable = false)
+    @Column(name = "FechaNacimiento", nullable = false)
     private Date fechaNacimiento;
-    @Column(name = "Email",nullable = false)
+    @Column(name = "Email", nullable = false)
     private String email;
-    @Column(name = "NumCelular",length = 15,nullable = false)
+    @Column(name = "NumCelular", length = 15, nullable = false)
     private String numCelular;
-    @Column(name = "Direccion",length = 50,nullable = false)
+    @Column(name = "Direccion", length = 50, nullable = false)
     private String direccion;
-    @Column(name = "Contrasena",length = 40,nullable = false)
+    @Column(name = "Contrasena", length = 40, nullable = false)
     private String contrasena;
-    @Column(name = "TipoUsuario",length = 50,nullable = false)
+    @Column(name = "TipoUsuario", length = 50, nullable = false)
     private String tipoUsuario;
 
     @OneToMany(mappedBy = "Usuarios")
