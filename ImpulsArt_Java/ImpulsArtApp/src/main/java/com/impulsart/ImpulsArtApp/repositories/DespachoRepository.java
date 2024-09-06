4b74ba8bedc510fc7fe0aa25daa9046a78b7b93b
shadowsouls444 <angel.acuna.meza@gmail.com>
2024-09-06 11:55:04 -0500

    Cambio de estructura

--- ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/repositories/DespachoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.impulsart.ImpulsArtApp.repositories;
-
-import com.impulsart.ImpulsArtApp.entities.Despacho;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DespachoRepository extends JpaRepository<Despacho, Long> {
-}
--- ImpulsArt_Java/ImpulsArtApp/src/main/java/com/impulsart/ImpulsArtApp/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.impulsart.ImpulsArtApp.service;
-
-public interface EmailService {
-
-    void enviarCorreo(String destinatario, String asunto, String nombre, String mensaje);
-
-    void enviarCorreoSubasta(String destinatario, String asunto, String nombre, String mensaje);
-
-}
