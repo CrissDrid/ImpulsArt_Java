12aa821bf03e44e7d981b193ffee3af0d1afec1b
shadowsouls444 <angel.acuna.meza@gmail.com>
2024-09-07 14:55:12 -0500

    Permisos para leer imagenes

--- src/main/java/com/impulsart/ImpulsArtApp/imgConfig.java
+++ src/main/java/com/impulsart/ImpulsArtApp/imgConfig.java
@@ -5,14 +5,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class StaticResourceConfig implements WebMvcConfigurer {
-
-    private static final String IMAGE_DIRECTORY = "/src/main/java/com/impulsart/ImpulsArtApp/imagen/";
+public class imgConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/imagen/**")
-                .addResourceLocations("file:" + System.getProperty("user.dir") + IMAGE_DIRECTORY)
-                .setCachePeriod(0); // Configura el período de caché según sea necesario
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/src/main/java/com/impulsart/ImpulsArtApp/imagen/")
+                .setCachePeriod(0); // Deshabilitar la caché para desarrollo
     }
 }
+
--- src/main/java/com/impulsart/ImpulsArtApp/security/SecurityConfig.java
+++ src/main/java/com/impulsart/ImpulsArtApp/security/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
                         authorizeRequests
 
                                 // PERMISOS PARA RECURSOS ESTÁTICOS
-                                .requestMatchers("https://athletic-wholeness-production.up.railway.app/imagen/**").permitAll()
+                                .requestMatchers("/imagen/**").permitAll()
                                 // PERMISOS PARA RECURSOS ESTÁTICOS
 
                                 // PERMISOS PARA RECURSOS ESTÁTICOS
