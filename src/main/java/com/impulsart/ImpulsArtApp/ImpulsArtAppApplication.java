6020f8128404418b65768f02eb98a6991ae65551
shadowsouls444 <angel.acuna.meza@gmail.com>
2024-09-06 19:14:01 -0500

    Url

--- src/main/java/com/impulsart/ImpulsArtApp/ImpulsArtAppApplication.java
+++ src/main/java/com/impulsart/ImpulsArtApp/ImpulsArtAppApplication.java
@@ -14,18 +14,5 @@ public class ImpulsArtAppApplication {
 		SpringApplication.run(ImpulsArtAppApplication.class, args);
 	}
 
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
 
 }
